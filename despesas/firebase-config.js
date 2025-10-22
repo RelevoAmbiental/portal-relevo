ee6d3cddea21cdf86b0a7495934f63fdc0f106d6
RelevoAmbiental <samuel@relevo.eco.br>
2025-10-22 19:54:33 -0300

    Update Firebase config to include storage and auth
    Added storage and authentication services to Firebase configuration. Enabled Firestore persistence for offline data access.

--- despesas/firebase-config.js
+++ despesas/firebase-config.js
@@ -1,13 +1,23 @@
-// Configuração Firebase - SUAS CREDENCIAIS
+// despesas/firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyCDIrPqQs7S_E2UeDGPNeFCVYcv09JFoTs",
-  authDomain: "app-despesas-7029f.firebaseapp.com",
-  projectId: "app-despesas-7029f",
-  messagingSenderId: "843931176271",
-  appId: "1:843931176271:web:5cdafdd10bc28c3bd8893a"
+    apiKey: "AIzaSyCDIrPqQs7S_E2UeDGPNeFCVYcv09JFoTs",
+    authDomain: "app-despesas-7029f.firebaseapp.com",
+    projectId: "app-despesas-7029f",
+    storageBucket: "app-despesas-7029f.appspot.com",
+    messagingSenderId: "843931176271",
+    appId: "1:843931176271:web:5cdafdd10bc28c3bd8893a"
 };
 
 // Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+
+// Inicializar serviços
+const auth = firebase.auth();
 const db = firebase.firestore();
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxDfo_xoXo4POKqSw0W3piKWUutECoXvgy1upb3ChCp3_V_t92Fc6UlKg6SmW_q8DOJ/exec";
+const storage = firebase.storage();
+
+// Configuração do Firestore para dados offline
+db.enablePersistence()
+  .catch((err) => {
+      console.log('Persistência falhou: ', err);
+  });
