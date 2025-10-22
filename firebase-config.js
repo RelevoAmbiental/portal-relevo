d3d3807c19495c98afedbbb27fedda47265871d4
RelevoAmbiental <samuel@relevo.eco.br>
2025-10-22 15:45:13 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuração do PORTAL PRINCIPAL (autenticação)
+const firebaseConfig = {
+  apiKey: "AIzaSyCDIrPqQs7S_E2UeDGPNeFCVYcv09JFoTs",
+  authDomain: "app-despesas-7029f.firebaseapp.com",
+  projectId: "app-despesas-7029f", 
+  messagingSenderId: "843931176271",
+  appId: "1:843931176271:web:5cdafdd10bc28c3bd8893a"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
