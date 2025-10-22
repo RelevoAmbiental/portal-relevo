fa3e4cd0f3cc4704eb2d1a25bc4fc62c969426f3
RelevoAmbiental <samuel@relevo.eco.br>
2025-10-22 17:02:57 -0300

    Update firebase-config.js

--- despesas/firebase-config.js
+++ despesas/firebase-config.js
@@ -1,16 +1,13 @@
-// Configuração Firebase
+// Configuração Firebase - SUAS CREDENCIAIS
 const firebaseConfig = {
   apiKey: "AIzaSyCDIrPqQs7S_E2UeDGPNeFCVYcv09JFoTs",
   authDomain: "app-despesas-7029f.firebaseapp.com",
   projectId: "app-despesas-7029f",
-  storageBucket: "app-despesas-7029f.firebasestorage.app",
   messagingSenderId: "843931176271",
   appId: "1:843931176271:web:5cdafdd10bc28c3bd8893a"
 };
 
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Referências globais
 const db = firebase.firestore();
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzns8tQtMpdAfOo_cfmpUfTq1g3hBbGv-dCLIO3M3NRDcgQxUh_N8QYNO9k9ZV9hTlA/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxDfo_xoXo4POKqSw0W3piKWUutECoXvgy1upb3ChCp3_V_t92Fc6UlKg6SmW_q8DOJ/exec";
