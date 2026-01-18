60345318d39b085742212e964913e4443294d59c
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2026-01-18 14:09:43 -0300

    Add icons and new entry for 'Fichas de Campo'

--- apps/apps.config.js
+++ apps/apps.config.js
@@ -3,12 +3,21 @@ window.RELEVO_APPS = [
     id: "despesas",
     titulo: "Despesas",
     descricao: "Lançamentos e gestão de despesas",
-    url: "https://portal.relevo.eco.br/despesas/"
+    url: "https://portal.relevo.eco.br/despesas/",
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/Gastos.png"
   },
   {
     id: "relatorio_atividades",
     titulo: "Relatório de Atividades",
     descricao: "Registro diário de atividades (campo/escritório)",
-    url: "https://portal.relevo.eco.br/relatorio_atividades/"
+    url: "https://portal.relevo.eco.br/relatorio_atividades/",
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/Educa%C3%A7%C3%A3oAmbiental.png"
+  },
+  {
+    id: "fichas_campo",
+    titulo: "Fichas de Campo",
+    descricao: "Coleta de informações em campo",
+    url: "https://www.appsheet.com/newshortcut/fcb9385d-2a8a-4787-87e0-5387a5f04628",
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/ficha%20de%20campo.jpg"
   }
 ];
