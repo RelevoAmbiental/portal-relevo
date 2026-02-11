2591018c219ce0f990b471905c00ae72aa662218
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2026-02-10 22:39:40 -0300

    Update apps.config.js

--- apps/apps.config.js
+++ apps/apps.config.js
@@ -4,27 +4,27 @@ window.RELEVO_APPS = [
     titulo: "Despesas",
     descricao: "Lançamentos e gestão de despesas",
     url: "https://portal.relevo.eco.br/despesas/",
-    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/Gastos.png"
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/Gastos.png",
   },
   {
     id: "relatorio_atividades",
     titulo: "Relatório de Atividades",
     descricao: "Registro diário de atividades (campo/escritório)",
     url: "https://portal.relevo.eco.br/relatorio_atividades/",
-    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/Educa%C3%A7%C3%A3oAmbiental.png"
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/Educa%C3%A7%C3%A3oAmbiental.png",
   },
   {
     id: "fichas_campo",
     titulo: "Fichas de Campo",
     descricao: "Coleta de informações em campo",
     url: "https://www.appsheet.com/newshortcut/fcb9385d-2a8a-4787-87e0-5387a5f04628",
-    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/ficha%20de%20campo.jpg"
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/ficha%20de%20campo.jpg",
   },
   {
     id: "seguranca",
     titulo: "SST e Segurança",
     descricao: "Gestão de documentos e segurança jurídica",
     url: "https://drive.google.com/drive/folders/12fEU0bPCOT8ThPQWYEZjt2opdXjJdOuG",
-    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/SegurancaJuridica.png"
-  }
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/SegurancaJuridica.png",
+  },
 ];
