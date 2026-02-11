de54017b80c45eeeed7774bed53e5b4864fc1376
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2026-02-10 22:26:38 -0300

    Update apps.config.js

--- apps/apps.config.js
+++ apps/apps.config.js
@@ -20,4 +20,10 @@ window.RELEVO_APPS = [
     url: "https://www.appsheet.com/newshortcut/fcb9385d-2a8a-4787-87e0-5387a5f04628",
     icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/ficha%20de%20campo.jpg"
   }
+  {
+    titulo: "SST e Segurança",
+    descricao: "Gestão de documentos e segurança jurídica",
+    url: "https://drive.google.com/drive/folders/12fEU0bPCOT8ThPQWYEZjt2opdXjJdOuG",
+    icon: "https://raw.githubusercontent.com/RelevoAmbiental/portal-relevo/refs/heads/main/styles/icons/SegurancaJuridica.png"
+  }
 ];
