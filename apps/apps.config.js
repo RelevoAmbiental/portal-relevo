fb9875f83feeda8be73b69419a999ba5cc106f40
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2026-02-13 18:36:13 -0300

    Update apps.config.js

--- apps/apps.config.js
+++ apps/apps.config.js
@@ -19,5 +19,12 @@ window.RELEVO_APPS = [
     descricao: "Coleta de informações em campo",
     url: "https://www.appsheet.com/newshortcut/fcb9385d-2a8a-4787-87e0-5387a5f04628",
     icon: "/styles/icons/ficha de campo.jpg"
+  },
+  {
+  id: "sst",
+  titulo: "SST e Segurança",
+  descricao: "Pasta de SST e Segurança (Drive)",
+  url: "https://drive.google.com/drive/folders/12fEU0bPCOT8ThPQWYEZjt2opdXjJdOuG",
+  icon: "/assets/icons/SegurancaJuridica.png"
   }
 ];
